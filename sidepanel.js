d91c08b396fa5e6199ea633701d097c88487ab37
Aleksandar Perisic <fizikaneri@gmail.com>
2024-05-22 16:17:47 +0200

    countsByTabId

--- sidepanel.js
+++ sidepanel.js
@@ -2,11 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     const activeTabId = tabs[0].id;
     chrome.storage.local.get(['countsByTabId'], (result) => {
-      const counts = result.countsByTabId ? result.countsByTabId[activeTabId] : {};
-      document.getElementById('h1-count').textContent = `H1 Count: ${counts.h1Count ?? 0}`;
-      document.getElementById('h2-count').textContent = `H2 Count: ${counts.h2Count ?? 0}`;
-      document.getElementById('h3-count').textContent = `H3 Count: ${counts.h3Count ?? 0}`;
-      document.getElementById('h4-count').textContent = `H4 Count: ${counts.h4Count ?? 0}`;
+      const countsByTabId = result.countsByTabId || {};
+      const counts = countsByTabId[activeTabId] || { h1Count: 0, h2Count: 0, h3Count: 0, h4Count: 0 };
+
+      document.getElementById('h1-count').textContent = `H1 Count: ${counts.h1Count}`;
+      document.getElementById('h2-count').textContent = `H2 Count: ${counts.h2Count}`;
+      document.getElementById('h3-count').textContent = `H3 Count: ${counts.h3Count}`;
+      document.getElementById('h4-count').textContent = `H4 Count: ${counts.h4Count}`;
     });
   });
 });
